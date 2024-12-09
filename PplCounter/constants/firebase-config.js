91428576da1df6b23868579e5a49a775c877a78f
Jordan Grewe <114885319+Grewestr@users.noreply.github.com>
2024-12-08 22:11:38 -0500

    Update firebase-config.js

--- PplCounter/constants/firebase-config.js
+++ PplCounter/constants/firebase-config.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase configuration from your Firebase Console
 const firebaseConfig = {
-  apiKey: "AIzaSyAj0-1YR0z8bccdZUlC10k0181wMSyj58",
-  authDomain: "peoplecounter-8c4fd.firebaseapp.com",
-  projectId: "peoplecounter-8c4fd",
-  storageBucket: "peoplecounter-8c4fd.appspot.com",
-  messagingSenderId: "508353821672",
-  appId: "1:508353821672:web:e14f88760b605a0d50825d",
-  measurementId: "G-DRBQX64EJH",
+  apiKey: "REDACTED",
+  authDomain: "REDACTED",
+  projectId: "REDACTED",
+  storageBucket: "REDACTED",
+  messagingSenderId: "REDACTED",
+  appId: "REDACTED",
+  measurementId: "REDACTED",
 };
 
 // Initialize Firebase
